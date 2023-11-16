69a1adb5c55dd4bf7c65732a3d6aedf1ca79f504
nateykliu <nathanykliu@gmail.com>
2023-11-16 09:34:09 -0800

    new

--- src/App.js
+++ src/App.js
@@ -14,3 +14,31 @@ const App = () => {
 };
 
 export default App;
+
+const express = require('express');
+const { Pool } = require('pg');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  // Additional database configuration as needed
+});
+
+app.get('/api/goals', async (req, res) => {
+  try {
+    const { rows } = await pool.query('SELECT * FROM goals');
+    res.json(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
