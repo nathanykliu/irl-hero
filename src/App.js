5e6c88d982ba5c75a76e738b66b56543b2206886
nateykliu <nathanykliu@gmail.com>
2023-11-17 10:40:10 -0800

    bg

--- src/App.js
+++ src/App.js
@@ -1,38 +1,30 @@
 import './App.css';
 import React from 'react';
-import MicroModal from 'micromodal';
 import GameCanvas from './components/GameCanvas';
-
 document.addEventListener('DOMContentLoaded', (event) => {
-  // Get the modal
-  var modal = document.getElementById("modal");
+  var intromodal = document.getElementById("intromodal");
+  var closeButton = document.getElementsByClassName("intromodal-close")[0];
 
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
-
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-    modal.style.display = "none";
+  closeButton.onclick = function() {
+    intromodal.style.display = "none";
   }
 
-  // When the user clicks anywhere outside of the modal, close it
   window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
+    if (event.target === intromodal) {
+      intromodal.style.display = "none";
     }
   }
 
-  // Close modal when pressing Escape key
   window.addEventListener('keydown', function (event) {
     if (event.key === 'Escape') {
-      modal.style.display = "none";
+      intromodal.style.display = "none";
     }
   });
 
-  // Display the modal by default
-  modal.style.display = "block";
+  intromodal.style.display = "block";
 });
 
+//load irl-hero 
 const App = () => {
   return (
       <div className="App">
