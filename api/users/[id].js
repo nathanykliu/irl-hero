67cfab94df7239ce6253f7e20fff037dda09b6ba
nateykliu <nathanykliu@gmail.com>
2023-11-17 09:28:13 -0800

    routes

--- api/users/[id].js
+++ api/users/[id].js
@@ -13,17 +13,19 @@ const pool = new Pool({
 });
 
 // GET one user by ID
-app.get('/api/getuser/byid', async (req, res) => {
-  const userid = 1
-  
-  // make sure that userId is a number
+app.get('/api/users/:id', async (req, res) => {
+  const userid = req.params.id;
+
+  // validate that userId is a number
   if (isNaN(userid)) {
     return res.status(400).send('Invalid user ID');
   }
 
   try {
     const result = await pool.query('SELECT * FROM users WHERE id = $1', [userid]);
-
+    if (result.rows.length === 0) {
+      return res.status(404).send('User not found');
+    }
     res.json(result.rows[0]);
   } catch (err) {
     console.error('Error fetching user:', err);
