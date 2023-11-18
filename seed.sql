797efbcd7a1acd1ae5a87358fa7ab327de617f1a
nateykliu <nathanykliu@gmail.com>
2023-11-18 15:00:35 -0800

    music

--- seed.sql
+++ seed.sql
@@ -12,13 +12,13 @@
 -- );
 
 -- create teh goals table
-CREATE TABLE goals (
-    id SERIAL PRIMARY KEY,
-    goals VARCHAR(255),
-    days INTEGER,
-    complete BOOLEAN,
-    user_id VARCHAR(255)
-);
+-- CREATE TABLE goals (
+--     id SERIAL PRIMARY KEY,
+--     goals VARCHAR(255),
+--     days INTEGER,
+--     complete BOOLEAN,
+--     user_id VARCHAR(255)
+-- );
 
 -- populate the table
 -- INSERT INTO users (lastname, firstname, stage) VALUES
@@ -36,13 +36,7 @@ CREATE TABLE goals (
 
 -- populate goals
 -- INSERT INTO goals (Goals, Days, Complete, User_id) VALUES
---     ('Complete MVP', 30, false, 1),
---     ('Eat lots of dog food', 60, false, 2),
---     ('Run a half marathon', 120, false, 3),
---     ('Read 12 books this year', 365, false, 4),
+--     ('Become the top Dog', 365, false, 1),
+--     ('Escape Full Stack Island', 30, false, 1),
 --     ('Practice dog yoga daily', 90, true, 5),
---     ('Volunteer 10 hours at the dog shelter', 200, false, 6),
---     ('Write a blog about this island', 45, false, 7),
---     ('Develop the mobile version of irl-hero', 180, false, 8),
---     ('Buy new Computer', 60, true, 9),
---     ('Fix my backend routes for this mvp', 150, false, 10);
+--     ('Buy new Computer', 60, true, 9)
